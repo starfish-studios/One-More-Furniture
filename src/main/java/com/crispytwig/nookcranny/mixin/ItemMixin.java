9c44d8dcdda3c1ed3bdc806b29708e9c7ca202bd
mrsterner <simon@sterner.dev>
2025-03-31 11:12:06 +0200

    item merge mixin tweak

--- src/main/java/com/crispytwig/nookcranny/mixin/ItemMixin.java
+++ src/main/java/com/crispytwig/nookcranny/mixin/ItemMixin.java
@@ -10,6 +10,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,6 +20,8 @@ import java.util.List;
 
 @Mixin(Item.class)
 public abstract class ItemMixin {
+
+    @Shadow public abstract String toString();
 
     @Inject(method = "appendHoverText", at = @At("TAIL"))
     private void addMailboxTooltip(ItemStack stack, Level world, List<Component> tooltip, TooltipFlag flag, CallbackInfo ci) {
@@ -64,6 +67,9 @@ public abstract class ItemMixin {
         if (tag != null) {
             tag.remove("mailboxTooltip");
             tag.remove("mailboxTooltipTimer");
+            if (tag.isEmpty()) {
+                stack.setTag(null);
+            }
         }
     }
 }
