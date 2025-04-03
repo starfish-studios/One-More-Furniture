9f9682671a529f52e0dc53f0853e2811b974ac7e
mrsterner <simon@sterner.dev>
2025-04-03 12:52:49 +0200

    Match old branch updates

--- common/src/main/java/com/crispytwig/omf/block/ShelfBlock.java
+++ common/src/main/java/com/crispytwig/omf/block/ShelfBlock.java
@@ -9,32 +9,38 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.Containers;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.*;
+import net.minecraft.world.level.block.state.properties.AttachFace;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.block.state.properties.SlabType;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class ShelfBlock extends BaseEntityBlock implements SimpleWaterloggedBlock {
-//    public static final EnumProperty<HorizontalConnectionType> TYPE = NCBlockProperties.HORIZONTAL_CONNECTION_TYPE;
     public static final EnumProperty<SlabType> HALF = EnumProperty.create("half", SlabType.class);
     public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
     public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
-
     public static final EnumProperty<AttachFace> FACE = BlockStateProperties.ATTACH_FACE;
-
     public static final VoxelShape NORTH_TOP_AAAB = Block.box(0.0, 8.0, 9.0, 16.0, 16.0, 16.0);
     public static final VoxelShape NORTH_BOTTOM_AABB = Block.box(0.0, 0.0, 9.0, 16.0, 8.0, 16.0);
     public static final VoxelShape EAST_TOP_AAAB = Block.box(0.0, 8.0, 0.0, 7.0, 16.0, 16.0);
@@ -43,19 +49,14 @@ public class ShelfBlock extends BaseEntityBlock implements SimpleWaterloggedBloc
     public static final VoxelShape SOUTH_BOTTOM_AABB = Block.box(0.0, 0.0, 0.0, 16.0, 8.0, 7.0);
     public static final VoxelShape WEST_TOP_AAAB = Block.box(9.0, 8.0, 0.0, 16.0, 16.0, 16.0);
     public static final VoxelShape WEST_BOTTOM_AABB = Block.box(9.0, 0.0, 0.0, 16.0, 8.0, 16.0);
-
-
     public static final VoxelShape NORTH_FLOOR_AABB = Block.box(0.0, 0.0, 4.0, 16.0, 16.0, 12.0);
     public static final VoxelShape WEST_FLOOR_AABB = Block.box(4.0, 0.0, 0.0, 12.0, 16.0, 16.0);
     public static final VoxelShape SOUTH_FLOOR_AABB = Block.box(0.0, 0.0, 4.0, 16.0, 16.0, 12.0);
     public static final VoxelShape EAST_FLOOR_AABB = Block.box(4.0, 0.0, 0.0, 12.0, 16.0, 16.0);
-
-
     public static final VoxelShape NORTH_FLOOR_SHORT_AABB = Block.box(0.0, 0.0, 4.0, 16.0, 8.0, 12.0);
     public static final VoxelShape WEST_FLOOR_SHORT_AABB = Block.box(4.0, 0.0, 0.0, 12.0, 8.0, 16.0);
     public static final VoxelShape SOUTH_FLOOR_SHORT_AABB = Block.box(0.0, 0.0, 4.0, 16.0, 8.0, 12.0);
     public static final VoxelShape EAST_FLOOR_SHORT_AABB = Block.box(4.0, 0.0, 0.0, 12.0, 8.0, 16.0);
-
     public static final VoxelShape NORTH_CEILING_AABB = Block.box(0.0, 8.0, 4.0, 16.0, 16.0, 12.0);
     public static final VoxelShape WEST_CEILING_AABB = Block.box(4.0, 8.0, 0.0, 12.0, 16.0, 16.0);
     public static final VoxelShape SOUTH_CEILING_AABB = Block.box(0.0, 8.0, 4.0, 16.0, 16.0, 12.0);
@@ -63,20 +64,16 @@ public class ShelfBlock extends BaseEntityBlock implements SimpleWaterloggedBloc
 
     public ShelfBlock(Properties properties) {
         super(properties);
-        this.defaultBlockState()
-//                .setValue(TYPE, HorizontalConnectionType.SINGLE)
-                .setValue(HALF, SlabType.BOTTOM)
-                .setValue(FACING, Direction.NORTH)
-                .setValue(FACE, AttachFace.WALL)
-                .setValue(WATERLOGGED, false);
+        this.defaultBlockState().setValue(HALF, SlabType.BOTTOM).setValue(FACING, Direction.NORTH).setValue(FACE, AttachFace.WALL).setValue(WATERLOGGED, false);
     }
+
     @Nullable
     @Override
     public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
         return new ShelfBlockEntity(pos, state);
     }
 
-    public RenderShape getRenderShape(BlockState state) {
+    public @NotNull RenderShape getRenderShape(BlockState state) {
         return RenderShape.MODEL;
     }
 
@@ -84,54 +81,97 @@ public class ShelfBlock extends BaseEntityBlock implements SimpleWaterloggedBloc
     public boolean canBeReplaced(BlockState state, BlockPlaceContext useContext) {
         ItemStack itemStack = useContext.getItemInHand();
         SlabType slabType = state.getValue(HALF);
-        if (slabType == SlabType.DOUBLE || !itemStack.is(this.asItem())) return false;
-
-        if (!useContext.replacingClickedOnBlock()) return true;
-
+        if (slabType == SlabType.DOUBLE || !itemStack.is(this.asItem()))
+            return false;
+        if (!useContext.replacingClickedOnBlock())
+            return true;
         Direction direction = useContext.getClickedFace();
-        if (slabType == SlabType.BOTTOM) return direction == Direction.UP || direction.getAxis().isHorizontal();
-
+        if (slabType == SlabType.BOTTOM)
+            return direction == Direction.UP || direction.getAxis().isHorizontal();
         return direction == Direction.DOWN || direction.getAxis().isHorizontal();
     }
 
     @Override
-    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
-
+    public @NotNull InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
         BlockEntity blockentity = level.getBlockEntity(pos);
-        if (!(blockentity instanceof ShelfBlockEntity shelfBE)) return InteractionResult.PASS;
-
+        if (!(blockentity instanceof ShelfBlockEntity shelfBE))
+            return InteractionResult.PASS;
         Direction facing = state.getValue(FACING);
         int slot = BlockPart.get2D(pos, hit.getLocation(), Direction.UP, facing.getClockWise(), 2, 2);
-
-        ItemStack stack = player.getItemInHand(hand);
-        if (!stack.isEmpty() && !stack.is(this.asItem())) {
-            if (!level.isClientSide && shelfBE.placeItem(player.getAbilities().instabuild ? stack.copy() : stack, slot)) {
-                level.playSound(null, pos, SoundEvents.ITEM_FRAME_ADD_ITEM, SoundSource.BLOCKS, 1.0F, 1.0F);
+        ItemStack handStack = player.getItemInHand(hand);
+        ItemStack shelfStack = shelfBE.getItems().get(slot);
+        if (handStack.is(this.asItem()))
+            return InteractionResult.PASS;
+        if (handStack.isEmpty()) {
+            if (!shelfStack.isEmpty()) {
+                player.setItemSlot(EquipmentSlot.MAINHAND, shelfStack.copy());
+                shelfBE.getItems().set(slot, ItemStack.EMPTY);
+                shelfBE.markUpdated();
+                level.playSound(null, pos, SoundEvents.ITEM_PICKUP, SoundSource.BLOCKS, 1.0F, 1.0F);
                 return InteractionResult.SUCCESS;
+            } else {
+                return InteractionResult.PASS;
             }
-            return InteractionResult.CONSUME;
         }
+        if (shelfStack.isEmpty()) {
+            shelfBE.getItems().set(slot, handStack.split(handStack.getCount()));
+            shelfBE.markUpdated();
+            level.playSound(null, pos, SoundEvents.ITEM_FRAME_ADD_ITEM, SoundSource.BLOCKS, 1.0F, 1.0F);
+            return InteractionResult.SUCCESS;
+        }
+        if (ItemStack.isSameItem(handStack, shelfStack)) {
+            int room = shelfStack.getMaxStackSize() - shelfStack.getCount();
+            if (room <= 0)
+                return InteractionResult.PASS;
+            int transfer = Math.min(handStack.getCount(), room);
+            handStack.shrink(transfer);
+            shelfStack.grow(transfer);
+            shelfBE.markUpdated();
+            level.playSound(null, pos, SoundEvents.ITEM_FRAME_ADD_ITEM, SoundSource.BLOCKS, 1.0F, 1.0F);
+            return InteractionResult.SUCCESS;
+        } else {
+            ItemStack temp = shelfStack.copy();
+            shelfBE.getItems().set(slot, handStack.copy());
+            player.setItemSlot(EquipmentSlot.MAINHAND, temp);
+            shelfBE.markUpdated();
+            level.playSound(null, pos, SoundEvents.ITEM_PICKUP, SoundSource.BLOCKS, 1.0F, 1.0F);
+            return InteractionResult.SUCCESS;
+        }
+    }
 
-        if (shelfBE.removeItem(slot, player, level)) return InteractionResult.SUCCESS;
-
-        return InteractionResult.PASS;
+    @Override
+    public void attack(BlockState state, Level level, BlockPos pos, Player player) {
+        BlockEntity blockentity = level.getBlockEntity(pos);
+        if (!(blockentity instanceof ShelfBlockEntity shelfBE))
+            return;
+        Vec3 eyePos = player.getEyePosition(1.0F);
+        Vec3 lookVec = player.getLookAngle();
+        Vec3 end = eyePos.add(lookVec.scale(5.0));
+        ClipContext context = new ClipContext(eyePos, end, ClipContext.Block.OUTLINE, ClipContext.Fluid.NONE, player);
+        BlockHitResult hitResult = level.clip(context);
+        if (!pos.equals(hitResult.getBlockPos()))
+            return;
+        Direction facing = state.getValue(FACING);
+        int slot = BlockPart.get2D(pos, hitResult.getLocation(), Direction.UP, facing.getClockWise(), 2, 2);
+        boolean removeAll = player.isShiftKeyDown();
+        if (shelfBE.removeItem(slot, player, level, removeAll))
+            level.playSound(null, pos, SoundEvents.ITEM_PICKUP, SoundSource.BLOCKS, 1.0F, 1.0F);
     }
 
     @Override
     public void onRemove(BlockState state, Level level, BlockPos pos, BlockState newState, boolean isMoving) {
-        if (state.is(newState.getBlock())) return;
-
+        if (state.is(newState.getBlock()))
+            return;
         BlockEntity blockentity = level.getBlockEntity(pos);
-        if (blockentity instanceof ShelfBlockEntity shelfBE) Containers.dropContents(level, pos, shelfBE.getItems());
+        if (blockentity instanceof ShelfBlockEntity shelfBE)
+            Containers.dropContents(level, pos, shelfBE.getItems());
         super.onRemove(state, level, pos, newState, isMoving);
     }
 
     @Override
-    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+    public @NotNull VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
         Direction direction = state.getValue(FACING);
-
         VoxelShape shape = Shapes.empty();
-
         if (state.getValue(FACE) == AttachFace.FLOOR) {
             if (state.getValue(HALF) == SlabType.BOTTOM) {
                 return switch (direction) {
@@ -141,7 +181,6 @@ public class ShelfBlock extends BaseEntityBlock implements SimpleWaterloggedBloc
                     default -> SOUTH_FLOOR_SHORT_AABB;
                 };
             }
-
             return switch (direction) {
                 case EAST -> EAST_FLOOR_AABB;
                 case WEST -> WEST_FLOOR_AABB;
@@ -149,7 +188,6 @@ public class ShelfBlock extends BaseEntityBlock implements SimpleWaterloggedBloc
                 default -> SOUTH_FLOOR_AABB;
             };
         }
-
         if (state.getValue(FACE) == AttachFace.CEILING) {
             if (state.getValue(HALF) == SlabType.TOP) {
                 return switch (direction) {
@@ -159,7 +197,6 @@ public class ShelfBlock extends BaseEntityBlock implements SimpleWaterloggedBloc
                     default -> SOUTH_CEILING_AABB;
                 };
             }
-
             return switch (direction) {
                 case EAST -> EAST_FLOOR_AABB;
                 case WEST -> WEST_FLOOR_AABB;
@@ -167,64 +204,49 @@ public class ShelfBlock extends BaseEntityBlock implements SimpleWaterloggedBloc
                 default -> SOUTH_FLOOR_AABB;
             };
         }
-
         if (state.getValue(HALF) == SlabType.TOP) {
-            if (direction == Direction.NORTH) shape = NORTH_TOP_AAAB;
-            if (direction == Direction.EAST) shape = EAST_TOP_AAAB;
-            if (direction == Direction.SOUTH) shape = SOUTH_TOP_AAAB;
-            if (direction == Direction.WEST) shape = WEST_TOP_AAAB;
+            if (direction == Direction.NORTH)
+                shape = NORTH_TOP_AAAB;
+            if (direction == Direction.EAST)
+                shape = EAST_TOP_AAAB;
+            if (direction == Direction.SOUTH)
+                shape = SOUTH_TOP_AAAB;
+            if (direction == Direction.WEST)
+                shape = WEST_TOP_AAAB;
         } else if (state.getValue(HALF) == SlabType.BOTTOM) {
-            if (direction == Direction.NORTH) shape = NORTH_BOTTOM_AABB;
-            if (direction == Direction.EAST) shape = EAST_BOTTOM_AABB;
-            if (direction == Direction.SOUTH) shape = SOUTH_BOTTOM_AABB;
-            if (direction == Direction.WEST) shape = WEST_BOTTOM_AABB;
+            if (direction == Direction.NORTH)
+                shape = NORTH_BOTTOM_AABB;
+            if (direction == Direction.EAST)
+                shape = EAST_BOTTOM_AABB;
+            if (direction == Direction.SOUTH)
+                shape = SOUTH_BOTTOM_AABB;
+            if (direction == Direction.WEST)
+                shape = WEST_BOTTOM_AABB;
         } else {
-            if (direction == Direction.NORTH) shape = Shapes.or(NORTH_TOP_AAAB, NORTH_BOTTOM_AABB);
-            if (direction == Direction.EAST) shape = Shapes.or(EAST_TOP_AAAB, EAST_BOTTOM_AABB);
-            if (direction == Direction.SOUTH) shape = Shapes.or(SOUTH_TOP_AAAB, SOUTH_BOTTOM_AABB);
-            if (direction == Direction.WEST) shape = Shapes.or(WEST_TOP_AAAB, WEST_BOTTOM_AABB);
+            if (direction == Direction.NORTH)
+                shape = Shapes.or(NORTH_TOP_AAAB, NORTH_BOTTOM_AABB);
+            if (direction == Direction.EAST)
+                shape = Shapes.or(EAST_TOP_AAAB, EAST_BOTTOM_AABB);
+            if (direction == Direction.SOUTH)
+                shape = Shapes.or(SOUTH_TOP_AAAB, SOUTH_BOTTOM_AABB);
+            if (direction == Direction.WEST)
+                shape = Shapes.or(WEST_TOP_AAAB, WEST_BOTTOM_AABB);
         }
         return shape;
     }
 
-//    @Override
-//    public BlockState updateShape(BlockState state, Direction direction, BlockState neighborState, LevelAccessor level, BlockPos currentPos, BlockPos neighborPos) {
-//        if (state.getValue(WATERLOGGED)) level.scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickDelay(level));
-//
-//        BlockState above = level.getBlockState(currentPos.above());
-//        if (direction == Direction.UP && (above.isFaceSturdy(level, currentPos, Direction.DOWN) && !above.getVisualShape(level, currentPos.above(), CollisionContext.empty()).isEmpty())) {
-//            if (level.getBlockEntity(currentPos) instanceof ShelfBlockEntity shelfBE) shelfBE.removeAllItems();
-//        }
-//
-//        Direction facing = state.getValue(FACING);
-//        Direction dirL = facing.getClockWise();
-//        Direction dirR = facing.getCounterClockWise();
-//        if (direction != dirL && direction != dirR) return state;
-//
-//        BlockState l_state = level.getBlockState(currentPos.relative(dirL));
-//        BlockState r_state = level.getBlockState(currentPos.relative(dirR));
-//        boolean l_side = l_state.getBlock() instanceof ShelfBlock && l_state.getValue(FACING) == facing;
-//        boolean r_side = r_state.getBlock() instanceof ShelfBlock && r_state.getValue(FACING) == facing;
-//        HorizontalConnectionType type = l_side && r_side ?
-//                HorizontalConnectionType.MIDDLE
-//                : (r_side ? HorizontalConnectionType.LEFT
-//                : (l_side ? HorizontalConnectionType.RIGHT
-//                : HorizontalConnectionType.SINGLE));
-//        return state.setValue(TYPE, type);
-//    }
-
     @Override
-    public FluidState getFluidState(BlockState state) {
+    public @NotNull FluidState getFluidState(BlockState state) {
         return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
     }
 
     @Override
-    public BlockState rotate(BlockState state, Rotation rotation) {
+    public @NotNull BlockState rotate(BlockState state, Rotation rotation) {
         return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
     }
 
     @Override
-    public BlockState mirror(BlockState state, Mirror mirror) {
+    public @NotNull BlockState mirror(BlockState state, Mirror mirror) {
         return state.rotate(mirror.getRotation(state.getValue(FACING)));
     }
 
@@ -233,31 +255,24 @@ public class ShelfBlock extends BaseEntityBlock implements SimpleWaterloggedBloc
     public BlockState getStateForPlacement(BlockPlaceContext context) {
         BlockPos clickedPos = context.getClickedPos();
         BlockState stateExisting = context.getLevel().getBlockState(clickedPos);
-
-        if (stateExisting.is(this)) return stateExisting.setValue(HALF, SlabType.DOUBLE);
+        if (stateExisting.is(this))
+            return stateExisting.setValue(HALF, SlabType.DOUBLE);
         BlockState blockState = this.defaultBlockState();
-
         boolean waterlogged = context.getLevel().getFluidState(clickedPos).getType() == Fluids.WATER;
-        BlockState state = blockState
-                .setValue(HALF, SlabType.BOTTOM)
-                .setValue(FACING, context.getHorizontalDirection().getOpposite())
-                .setValue(WATERLOGGED, waterlogged);
+        BlockState state = blockState.setValue(HALF, SlabType.BOTTOM).setValue(FACING, context.getHorizontalDirection().getOpposite()).setValue(WATERLOGGED, waterlogged);
         Direction direction = context.getClickedFace();
         if (direction == Direction.DOWN) {
             state = state.setValue(FACE, AttachFace.CEILING);
         } else if (direction == Direction.UP) {
-            state = state
-                    .setValue(FACE, AttachFace.FLOOR)
-                    .setValue(HALF, SlabType.BOTTOM);
+            state = state.setValue(FACE, AttachFace.FLOOR).setValue(HALF, SlabType.BOTTOM);
         } else {
             state = state.setValue(FACE, AttachFace.WALL);
         }
-        return direction != Direction.DOWN && (direction == Direction.UP || !(context.getClickLocation().y - (double)clickedPos.getY() > 0.5)) ? state : state.setValue(HALF, SlabType.TOP);
-
+        return direction != Direction.DOWN && (direction == Direction.UP || !(context.getClickLocation().y - (double) clickedPos.getY() > 0.5)) ? state : state.setValue(HALF, SlabType.TOP);
     }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(HALF, FACING, WATERLOGGED/*, TYPE*/, FACE);
+        builder.add(HALF, FACING, WATERLOGGED, FACE);
     }
 }
--- common/src/main/java/com/crispytwig/omf/block/entity/ShelfBlockEntity.java
+++ common/src/main/java/com/crispytwig/omf/block/entity/ShelfBlockEntity.java
@@ -1,23 +1,19 @@
 package com.crispytwig.omf.block.entity;
 
-import com.crispytwig.omf.block.ShelfBlock;
 import com.crispytwig.omf.registry.OMFBlockEntities;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.NonNullList;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
-import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.Clearable;
 import net.minecraft.world.ContainerHelper;
 import net.minecraft.world.Containers;
-import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.SlabType;
+import org.jetbrains.annotations.NotNull;
 
 public class ShelfBlockEntity extends BlockEntity implements Clearable {
     private final NonNullList<ItemStack> items = NonNullList.withSize(4, ItemStack.EMPTY);
@@ -49,79 +45,50 @@ public class ShelfBlockEntity extends BlockEntity implements Clearable {
     }
 
     @Override
-    public CompoundTag getUpdateTag() {
+    public @NotNull CompoundTag getUpdateTag() {
         CompoundTag compoundtag = new CompoundTag();
         ContainerHelper.saveAllItems(compoundtag, this.items, true);
         return compoundtag;
     }
 
 
-    public boolean placeItem(ItemStack stack, int position) {
-        ItemStack itemstack = this.items.get(position);
-        SlabType half = this.getBlockState().getValue(ShelfBlock.HALF);
+    public boolean removeItem(int index, Player player, Level level, boolean removeAll) {
+        ItemStack shelfStack = this.items.get(index);
+        if (shelfStack.isEmpty()) return false;
 
-        if (!itemstack.isEmpty()) return false;
+        int removeCount = removeAll ? shelfStack.getCount() : 1;
+        ItemStack removed = shelfStack.copy();
+        removed.setCount(removeCount);
 
-        if (half == SlabType.TOP && position == 0) this.items.set(0, stack.split(stack.getCount()));
-        else if (half == SlabType.TOP && position == 2) this.items.set(2, stack.split(stack.getCount()));
-        else if (half == SlabType.BOTTOM && position == 1) this.items.set(1, stack.split(stack.getCount()));
-        else if (half == SlabType.BOTTOM && position == 3) this.items.set(3, stack.split(stack.getCount()));
-        else if (half == SlabType.DOUBLE) this.items.set(position, stack.split(stack.getCount()));
-        else return false;
-
-        this.markUpdated();
-        return true;
-    }
-
-    public boolean removeItem(int index, Player player, Level level) {
-        if (this.items.get(index).isEmpty()) return false;
-
-        if (level.isClientSide()) {
-            player.playSound(SoundEvents.ITEM_PICKUP);
-            return true;
+        shelfStack.shrink(removeCount);
+        if (shelfStack.getCount() <= 0) {
+            this.items.set(index, ItemStack.EMPTY);
         }
 
-        ItemStack item = this.items.get(index).copy();
-        player.setItemSlot(EquipmentSlot.MAINHAND, item);
-        this.items.set(index, ItemStack.EMPTY);
+        if (!player.addItem(removed.copy())) {
+            Containers.dropItemStack(level, worldPosition.getX(), worldPosition.getY(), worldPosition.getZ(), removed);
+        }
+
         this.markUpdated();
         return true;
     }
 
-    public boolean dropItem(int index, Level level) {
+    public void dropItem(int index, Level level) {
         ItemStack item = this.items.get(index).copy();
         Containers.dropItemStack(level, worldPosition.getX(), worldPosition.getY(), worldPosition.getZ(), item);
         this.items.set(index, ItemStack.EMPTY);
         this.markUpdated();
-        return true;
     }
 
-    private void markUpdated() {
+    public void markUpdated() {
         this.setChanged();
-        this.getLevel().sendBlockUpdated(this.getBlockPos(), this.getBlockState(), this.getBlockState(), 3);
+        if (this.getLevel() != null) {
+            this.getLevel().sendBlockUpdated(this.getBlockPos(), this.getBlockState(), this.getBlockState(), 3);
+        }
     }
 
     public void clearContent() {
         this.items.clear();
     }
 
-    public void removeAllItems() {
-        boolean update = false;
-        for (int i = 0; i < items.size(); i++) {
-            if (this.items.get(i).isEmpty()) continue;
-            double posX = worldPosition.getX() + 0.3 + 0.4 * (i % 2);
-            double posY = worldPosition.getY() + 1.0;
-            double posZ = worldPosition.getZ() + 0.3 + 0.4 * (i / 2);
-
-            assert this.level != null;
-            ItemEntity entity = new ItemEntity(this.level, posX, posY + 0.1, posZ, this.items.get(i).copy());
-            this.level.addFreshEntity(entity);
-            this.items.set(i, ItemStack.EMPTY);
-            update = true;
-
-        }
-        if (update) {
-            this.markUpdated();
-        }
-    }
 }
